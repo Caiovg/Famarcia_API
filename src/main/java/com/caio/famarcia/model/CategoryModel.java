120cf6c7c6274475ffc383966277f42495b3f144
unknown <caioviniciusg.1000@gmail.com>
2021-08-31 11:46:16 -0300

    Modifying CategoryModel, Create CategoryController

--- src/main/java/com/caio/famarcia/model/CategoryModel.java
+++ src/main/java/com/caio/famarcia/model/CategoryModel.java
@@ -2,11 +2,15 @@ package com.caio.famarcia.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
+@Entity
+@Table(name = "category")
 public class CategoryModel implements Serializable{
 
 	private static final long serialVersionUID = 1L;
