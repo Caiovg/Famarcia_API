45d0a12875da2d429d17bffb3e317f95f322015e
unknown <caioviniciusg.1000@gmail.com>
2021-08-31 14:38:55 -0300

    creating the method findAllProduct

--- src/main/java/com/caio/famarcia/controller/ProductController.java
+++ src/main/java/com/caio/famarcia/controller/ProductController.java
@@ -1,12 +1,28 @@
 package com.caio.famarcia.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.caio.famarcia.model.ProductModel;
+import com.caio.famarcia.service.ProductServices;
 
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/product")
 public class ProductController {
 
+	@Autowired
+	private ProductServices service;
+	
+	@GetMapping
+	public ResponseEntity<List<ProductModel>> findAllProduct(){
+		List<ProductModel> obj = service.findAllProduct();
+		return ResponseEntity.ok().body(obj);
+	}
 }
--- src/main/java/com/caio/famarcia/service/ProductServices.java
+++ src/main/java/com/caio/famarcia/service/ProductServices.java
@@ -1,8 +1,20 @@
 package com.caio.famarcia.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.caio.famarcia.model.ProductModel;
+import com.caio.famarcia.repository.ProductRepository;
 
 @Service
 public class ProductServices {
 
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<ProductModel> findAllProduct() {
+		return repository.findAll();
+	}
 }
