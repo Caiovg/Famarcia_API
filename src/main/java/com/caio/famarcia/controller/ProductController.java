e1e62d82ce06ffc344c7dcb69dd61e227e0d9c8f
unknown <caioviniciusg.1000@gmail.com>
2021-08-31 14:27:34 -0300

    Creating ProductController

---
+++ src/main/java/com/caio/famarcia/controller/ProductController.java
@@ -0,0 +1,12 @@
+package com.caio.famarcia.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+}
