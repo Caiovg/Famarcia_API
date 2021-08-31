36b3da34fe449bcb8697b7d3771e02987056f6e1
unknown <caioviniciusg.1000@gmail.com>
2021-08-31 11:56:14 -0300

    creating the method findAllCategory

--- src/main/java/com/caio/famarcia/controller/CategoryController.java
+++ src/main/java/com/caio/famarcia/controller/CategoryController.java
@@ -1,12 +1,30 @@
 package com.caio.famarcia.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.caio.famarcia.model.CategoryModel;
+import com.caio.famarcia.service.CategoryServices;
+
+
 
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/category")
 public class CategoryController {
 
+	@Autowired
+	private CategoryServices service;
+
+	@GetMapping
+	public ResponseEntity<List<CategoryModel>> findAllCategory(){
+		List<CategoryModel> obj = service.findAllCategory();
+		return ResponseEntity.ok().body(obj);
+	}
 }
--- src/main/java/com/caio/famarcia/service/CategoryServices.java
+++ src/main/java/com/caio/famarcia/service/CategoryServices.java
@@ -1,5 +1,21 @@
 package com.caio.famarcia.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.caio.famarcia.model.CategoryModel;
+import com.caio.famarcia.repository.CategoryRepository;
+
+
+@Service
 public class CategoryServices {
 
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<CategoryModel> findAllCategory() {
+		return repository.findAll();
+	}
 }
